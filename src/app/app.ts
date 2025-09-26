b94e40809cdeac7134d152123ec96257544203a0
Milos Stevanovic <stevanovicmilos0003@gmail.com>
2025-09-26 23:51:25 +0200

    gtag setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-import { Toolbar } from "./toolbar/toolbar";
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { Toolbar } from './toolbar/toolbar'; // Ensure Toolbar is a standalone component
 import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs';
+
+declare var gtag: (...args: any[]) => void;
 
 @Component({
   selector: 'app-root',
@@ -12,11 +15,18 @@ import { CommonModule } from '@angular/common';
 export class App {
   protected title = 'BearlyBlogging_Front';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    // Subscribe to router events to track page views with Google Analytics
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      gtag('config', 'G-89D9H9VN85', {
+        'page_path': event.urlAfterRedirects
+      });
+    });
+  }
 
   showToolbar(): boolean {
-    return this.router.url == '/login' ||
-      this.router.url == '/register' ||
-      this.router.url == '/'
+    return !['/login', '/register', '/'].includes(this.router.url);
   }
 }
